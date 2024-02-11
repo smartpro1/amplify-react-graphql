82c40898a3354c5483fd123a8bd19fdc716c9c0a
smartpro1 <akeni.promise@yahoo.com>
2024-02-11 07:53:17 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React, Hello world!
+          Modified version, deployment made with Amazon Amplify!!!
         </a>
       </header>
     </div>
